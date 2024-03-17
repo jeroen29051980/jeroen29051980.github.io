4c3c27569358e4fa9ba11e4f27f20c8f707f834c
Jeroen Heymans <heymansjeroen@proximus.be>
2024-03-17 09:49:27 +0100

    Intermediate save Partial translation Adapted Javascript for opening Modal Adde latest education

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,19 @@
-let allModals=document.getElementsByClassName("modal");
+let allModals = document.getElementsByClassName("modal");
 
 function openModal(btnID) {
+    for (let i = 0; i < allModals.length; i++) {
+        allModals[i].style.display = "none";
+    }
     document.getElementById(btnID).style.display = "block";
 }
 
 function closeModal() {
     for (let i = 0; i < allModals.length; i++) {
-    allModals[i].style.display = "none";
-}
+        allModals[i].style.display = "none";
+    }
 }
 
 function changeLanguage() {
     // Implement language switching logic here
-    
+
 }
